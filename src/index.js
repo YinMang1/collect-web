d77c73352144156877fea8b6773f0e3015b3c46f
YinMang <xiabotao7033@sina.com>
2020-09-01 00:06:33 +0800

    Promise

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 
-Class Promise() {
-    
+const pending = 'pending'
+const fulfilled = 'fulfilled'
+const rejectd = 'rejected'
+Class Promise {
+    constructor(executor) {
+        this.status = pending
+    }
+    function resolve(value) {
+
+    }
+    function reject(value) {
+
+    }
+
+
 }
 
 
 module.exports = {
-    hello:'this is my world'
+    hello: 'this is my world'
 }
